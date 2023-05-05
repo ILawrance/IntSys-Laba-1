880b7b3dc61393194ee354f6da0367c9cf38e434
Igor Balashov <wowzawow@list.ru>
2023-05-05 21:43:32 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace IntSys_Laba_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(167, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Да";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Диагностика телевизора";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(19, 81);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(651, 92);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "Здравствуйте! \nВы открыли программу для быстрой диагностики неполадок телевизора." +
+    "\nДалее будут представлены вопросы, на которые необходимо будет ответить \"Да\" или" +
+    " \"Нет\".\nГотовы начать?";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(399, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 60);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 365);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Диагностика неполадок";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntSys_Laba_1
+{
+    public partial class Form1 : Form
+    {
+        public int etap = 0; //буду вести счет этапов, этап увеличивается после каждого уточняющего вопроса
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)    //соответсвует ответа "Да"
+        { 
+            switch (etap)  // в зависмости от этапа будет вызываться соответсвующий вопрос
+            {             // Qest везде ниже это разные вопросы, а Ans это ответы на них. номер кейса и номер ответа совпадают.
+                case 0:
+                    etap = Sobytiya.Qest1(richTextBox1, etap);    
+                    break;
+                case 1:
+                    Sobytiya.Ans1(richTextBox1);
+                    break;
+                case 2:
+                    etap = Sobytiya.Qest3(richTextBox1, etap);
+                    break;
+                case 3:
+                    Sobytiya.Ans3x1(richTextBox1);
+                    break;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e) // Соответсвует ответу "Нет" 
+        {
+            switch (etap)
+            {
+                case 0:
+                    this.Close(); 
+                    break;
+                case 1:
+                    etap = Sobytiya.Qest2(richTextBox1, etap);
+                    break;
+                case 2:
+                    Sobytiya.Ans2(richTextBox1);
+                    break;
+                case 3:
+                    Sobytiya.Ans3(richTextBox1);
+                    break;
+            }
+        }
+    }
+    public static class Sobytiya // класс, в котором храняться вопросы и ответы
+    {
+        public static int Qest1(RichTextBox richTextBox, int etap)
+        {
+            richTextBox.Text = "Сгорел ли предохранитель?";
+            return ++etap;
+        }
+        public static int Qest2(RichTextBox richTextBox, int etap)
+        {
+            richTextBox.Text = "Кабель питания целый?";
+            return ++etap;
+        }
+        public static int Qest3(RichTextBox richTextBox, int etap)
+        {
+            richTextBox.Text = "Вы разбираетесь в радиоэлектронике?";
+            return ++etap;
+        }
+        public static void Ans1(RichTextBox richTextBox)
+        {
+            richTextBox.Text = "Тогда выполните замену предохранителя.";
+        }
+        public static void Ans2(RichTextBox richTextBox)
+        {
+            richTextBox.Text = "Тогда выполните замену кабеля питания.";
+        }
+        public static void Ans3(RichTextBox richTextBox)
+        {
+            richTextBox.Text = "Тогда вызовите мастера.";
+        }
+        public static void Ans3x1(RichTextBox richTextBox)
+        {
+            richTextBox.Text = "Тогда выполните ремонт телевизора самостоятельно.";
+        }
+    }
+}
